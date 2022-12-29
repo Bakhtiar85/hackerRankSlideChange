ac37c5a8a12d5f19b5b4183676c67df861570eab
Bakhtiar85 <makhdoomabubakar85@gmail.com>
2022-12-29 21:03:45 +0500

    All done! Needs to check on different dataset.

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,17 +1,71 @@
-import React from 'react';
+import React, { useRef, useEffect, useReducer } from 'react';
 
-function Slides({slides}) {
+function reducer(state, action) {
+    switch (action.type) {
+        case 'incremented': {
+            return {
+                count: state.count + 1
+            };
+        }
+        case 'decremented': {
+            return {
+                count: state.count - 1
+            };
+        }
+        case 'default':
+            return {
+                count: 0
+            };
+    }
+}
+
+function Slides({ slides }) {
+    const initialState = { count: 0 };
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    function changeSlide(num) {
+        if (state.count == 0) {
+            prv.current.disabled = true;
+        }
+
+        if (state.count == 4) {
+            nxt.current.disabled = true;
+        }
+
+        if (num == 1 && state.count > 0) {
+            nxt.current.disabled = false;
+            dispatch({ type: 'decremented' });
+        } else if (num == 2 && state.count < 4) {
+            prv.current.disabled = false;
+            dispatch({ type: 'incremented' });
+        } else if (num == 0) {
+            nxt.current.disabled = false;
+            prv.current.disabled = true;
+            dispatch({ type: 'default' });
+        }
+
+    }
+
+    useEffect(() => {
+        console.log(slides);
+        changeSlide(0)
+    }, [])
+
+
+    const nxt = useRef();
+    const prv = useRef();
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick={() => changeSlide(0)}>Restart</button>
+                <button data-testid="button-prev" ref={prv} className="small" onClick={() => changeSlide(1)}>Prev</button>
+                <button data-testid="button-next" ref={nxt} className="small" onClick={() => changeSlide(2)}>Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 >{slides[state.count]['title']}</h1>
+                <p >{slides[state.count]['text']}</p>
             </div>
         </div>
     );
